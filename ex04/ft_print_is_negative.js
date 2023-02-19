69d600cef418fcf5d521db6c849d3dc6fa78a46d
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-02-19 19:48:17 +0100

    création des combinaisions trois chiffres

--- ex04/ft_print_is_negative.js
+++ ex04/ft_print_is_negative.js
@@ -1,9 +1,7 @@
-function ft_print_is_negative(n){
-    if (n<0){
+if (n<0){
         console.log("N");
     }
     else if (n>=0){
         console.log("P");
     }
-}
 ft_print_is_negative(-2)
--- ex05/ft_print_comb.js
+++ ex05/ft_print_comb.js
@@ -1,4 +1,16 @@
-function ft_print_comb(){
-    
+function comb_trois_chiffres(){
+    let a = 0;
+    while (a <= 7) {
+        let b = a + 1;
+        while (b <= 8) {
+            let c = b + 1;
+            while (c <= 9) {
+                console.log(`${a}${b}${c}`);
+                c++;
+            }
+            b++;
+        }
+        a++;
+    } 
 }
 
